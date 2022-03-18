f7b15413bf9d8b2a9715010ec9230a72ed348b74
francesg-code <75420565+francesg-code@users.noreply.github.com>
2022-03-18 18:32:48 +0000

    Reset ball direction if one player misses
    When a player misses hitting the ball with a paddle, the ball will now reset and change direction towards the other player.

--- main.py
+++ main.py
@@ -31,8 +31,16 @@ while game_is_on:
         ball.bounce_y()
 
     #Detect ball collison with paddle
-    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() <320:
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
+
+    #Detect when right paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    # Detect when left paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
 
 
 screen.exitonclick()
